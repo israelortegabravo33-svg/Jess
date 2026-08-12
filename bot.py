d6f62a71384a13d6f4edfaad50c2701390326756
israelortegabravo33-svg <israelortegabravo33@gmail.com>
2026-08-11 22:11:41 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,10 +11,11 @@ client = genai.Client(api_key=API_KEY)
 async def handle_message(update, context):
     try:
         # Usamos gemini-2.5-flash (o gemini-1.5-flash) con la nueva estructura
-        response = client.models.generate_content(
-            model='gemini-2.5-flash',
+                response = client.models.generate_content(
+            model='gemini-2.5',
             contents=update.message.text,
         )
+
         await update.message.reply_text(response.text)
     except Exception as e:
         await update.message.reply_text(f"Error en circuitos: {e}")
